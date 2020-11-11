54f640f00badd5abaab173d15e00d4f001e87d61
Pixle <pixlefury@outlook.com>
2020-11-11 18:39:54 +0000

    Shopify API work. Added shop.html and worked on the global stylesheet.

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,7 @@ const https = require("https"), path = require("path");
 const express = require("express");
 const { request } = require("express");
 const router = express.Router();
+const graphql = require("shopify");
 
 const api_settings = {
 	key: "021b74c9a0db6e2a2ae1521ee80ebd82",
@@ -10,28 +11,10 @@ const api_settings = {
 	shop: "peexee-corp-dev-store"
 }
 
-const auth = Buffer.from(`${api_settings.key}:${api_settings.password}`, "utf-8").toString("base64");
-const api_url = `https://${api_settings.shop}.myshopify.com/admin/api/${api_settings.version}/graphql.json`
-
-function query_api(query, callback) {
-	let req = https.request(api_url, {method: "POST"}, (res) => {
-		let data = "";
-
-		res.on("data", d => data += d)
-		res.on("end", () => {
-			console.log(data);
-			callback(JSON.parse(data))
-		})
-
-		res.on("error", (error) => {
-			console.log("" + error);
-		})
-	});
-
-	req.setHeader("Authorization", `Basic ${auth}`);
-	req.setHeader("Content-Type", "application/json");
-	req.end(JSON.stringify({query: query}))
-}
+const api = new graphql.ShopifyServer(
+	api_settings.key, api_settings.password,
+	api_settings.version, api_settings.shop
+)
 
 // Shop homepage
 router.get("/", (req, res) => {
@@ -40,97 +23,37 @@ router.get("/", (req, res) => {
 
 // Get all products
 router.get("/api/products", (req, res) => {
-	query_api(`
-		query {
-			shop {
-				products(first: 5) {
-					edges {
-						node {
-							id
-							title
-							descriptionHtml
-							featuredImage {originalSrc}
-							variants(first: 1) {
-								edges {
-									node {
-										price
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	`, data => {
-		let products = [];
-
-		data.data.shop.products.edges.forEach(product => {
-			products.push({
-				id: product.node.id,
-				title: product.node.title,
-				desc: product.node.descriptionHtml,
-				icon: product.node.featuredImage,
-				price: product.node.variants.edges[0].node.price
-			})
-		});
-
-		res.json(products);
-	});
+	res.json(api.products);
 })
 
 // Get indiviual product
 router.get("/api/product/:id", (req, res) => {
-	query_api(`
-		query {
-			product(id: "gid://shopify/Product/${req.params.id}") {
-				title
-				descriptionHtml
-				featuredImage {originalSrc}
-				variants(first: 1) {
-					edges {
-						node {
-							price
-						}
-					}
-				}
-			}
-		}
-	`, data => {
-		let product = {
-			id: req.params.id,
-			title: data.data.product.title,
-			desc: data.data.product.descriptionHtml,
-			icon: data.data.product.featuredImage,
-			price: data.data.product.variants.edges[0].node.price
-		};
-
+	product = api.get_product(req.params.id);
+	if (product != null) {
 		res.json(product);
-	});
+	} else {
+		res.status(404).send("Product not found!");
+	}
 })
 
 // Update product info
 router.post("/api/product/:id", (req, res) => {
-	query_api(`
-		mutation {
-			productUpdate(input: {
-				id: "gid://shopify/Product/${req.params.id}"
-				title: "${req.body.title}"
-				descriptionHtml: "${req.body.desc}"
-			}) {
-				userErrors {
-					field
-					message
-				}
-			}
-		}
-	`, data => {
-		res.json({});
-	});
+	let product = api.get_product(req.params.id);
+
+	if ("title" in req.body && req.body.title != null) {
+		product.set_title(req.body.title)
+	}
+	if ("desc" in req.body && req.body.desc != null) {
+		product.set_desc(req.body.desc)
+	}
+
+	api.update_product(req.params.id);
 })
 
 // Create product
 router.post("/api/create-product", (req, res) => {
+	let product = api.create_product(req.body.title, req.body.desc);
+
 	query_api(`
 		mutation {
 			productCreate(input: {
